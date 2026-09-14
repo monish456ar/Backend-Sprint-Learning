a2e5859eaba2a70565eff8f53120006fca1dd1fa
Monish Kumar <monish123ar@gmail.com>
2026-09-14 12:13:43 +0530

    made chages added collection method

--- Day2/pratice.py
+++ Day2/pratice.py
@@ -1,6 +1,13 @@
 import copy
 
 # list
+
+# comprehensions of list
+numbers = [1, 2, 3, 4]
+
+squares = [number * number for number in numbers]
+
+
 tasks = ["login", "dashboard", "profile"]
 
 tasks.append("settings")
@@ -11,6 +18,7 @@ print(tasks)
 location = (12.97, 77.59)
 
 print(location[0])
+# location[0]=22.09
 
 
 # dictionary
@@ -67,3 +75,29 @@ copy_user["name"] = "Rahul"
 
 print(user)
 print(copy_user)
+
+
+
+# deep copy
+import copy
+
+user = {
+    "name": "Monish",
+    "skills": ["Python", "JS"]
+}
+
+deep_copy_user = copy.deepcopy(user)
+
+deep_copy_user["skills"].append("Docker")
+
+print(user)
+print(deep_copy_user)
+
+
+
+# filter
+numbers = [10, 15, 20, 25, 30]
+
+even_numbers = list(filter(lambda number: number % 2 == 0, numbers))
+
+print(even_numbers)
